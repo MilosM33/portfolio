9693cf48336e96323a5ee8399fe7a9552d1b4dbf
Milos <61538921+MilosM33@users.noreply.github.com>
2022-05-30 18:28:18 +0200

    Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import "./Navbar.css";
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/about">About me</a>
+        </li>
+        <li>
+          <a href="/">Contact</a>
+        </li>
+        <li>
+          <a href="/about">Home</a>
+        </li>
+        <li>
+          <a href="/about">Portfolio</a>
+        </li>
+        <li>
+          <a href="/about">Blog</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import "./Home.css";
+import "../index.css";
+export default function Home() {
+  return (
+    <div className="background">
+      <Navbar></Navbar>
+    </div>
+  );
+}

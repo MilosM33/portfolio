4b921bd46e1ae27a5cdb711e4abca0655b39f9ec
Milos <61538921+MilosM33@users.noreply.github.com>
2022-05-30 18:54:26 +0200

    Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,19 @@ export default function Home() {
   return (
     <div className="background">
       <Navbar></Navbar>
+
+      <section className="hero">
+        <h1 className="text-lg">
+          Hello, I'm <span className="text-secondary">Miloš Marejka</span>
+        </h1>
+        <h2 className="text-md">
+          I'm a <span className="text-secondary">Full Stack Developer</span>
+        </h2>
+        <div className="btn-section">
+          <button className="btn btn-secondary">Hire me</button>
+          <button className="btn btn-primary">Contact me</button>
+        </div>
+      </section>
     </div>
   );
 }

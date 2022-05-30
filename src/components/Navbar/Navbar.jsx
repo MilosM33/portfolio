aeabb4f225039698ce471f65bedd1747e4742bec
Milos <61538921+MilosM33@users.noreply.github.com>
2022-05-30 19:25:51 +0200

    React icons, mobilenavbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import {FaHamburger} from "react-icons/fa";
 export default function Navbar() {
   let routes = [
     {
@@ -24,14 +25,20 @@ export default function Navbar() {
     }
   ];
 
+  function renderLinks() {
+    return routes.map((route, i) => (
+      <li>
+      <Link key={i} to={route.path}>
+        {route.name}
+      </Link>
+      </li>
+    ));
+  }
   return (
     <nav>
-      <ul>
-        {routes.map((route) => (
-          <li>
-            <Link to={"/about"}>{route.name}</Link>
-          </li>
-        ))}
+      <FaHamburger className="hamburger"></FaHamburger>
+      <ul className="navbar active">
+        {renderLinks()}
       </ul>
     </nav>
   );
